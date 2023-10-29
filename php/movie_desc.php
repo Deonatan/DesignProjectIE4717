8df95eaf9dd5cdaffd8312052677cad663dbf94a
hugosebastian <hugosebastian110@gmail.com>
2023-10-29 16:56:00 +0800

    add playing at info movie desc

--- php/movie_desc.php
+++ php/movie_desc.php
@@ -17,6 +17,31 @@
     $requested_id = $_GET['id'];
     $movie_query = "SELECT * FROM movie WHERE id like '$requested_id'";
     $movie_data = $db->query($movie_query)->fetch_assoc();
+
+    //Get Playing At info
+    $theatre_id_query = "SELECT theatre_id FROM movie_schedule WHERE movie_id=$requested_id;";
+    $theatre_id_result = $db->query($theatre_id_query);
+    if ($theatre_id_result) {
+        $theatre_ids = array(); // Create an array to store the movie IDs
+        while ($row = $theatre_id_result->fetch_assoc()) {
+            $theatre_ids[] = $row['theatre_id'];
+        }
+    }
+
+    // Convert the array of movie IDs to a comma-separated string
+    $theatre_id_list = implode(', ', $theatre_ids);
+
+    //Get Theatre Names
+    $theatre_name_query = "SELECT name FROM theatre WHERE id IN ($theatre_id_list)";
+    $theatre_name_result = $db->query($theatre_name_query);
+    $theatreNames = array(); // Create an array to store the theatre names
+    while ($row = $theatre_name_result->fetch_assoc()) {
+        $theatreNames[] = $row['name']; // Add each name to the array
+    }
+
+    // Convert the array to a comma-separated string
+    $theatreNamesString = implode(', ', $theatreNames);
+
     // foreach ($items as $key => $value) {
     //     echo $key . ": " . $value . "<br>";
     // } 
@@ -42,7 +67,9 @@
                         </div>
                     </div><br>
                     <strong>Synopsis</strong><br>
-                    <span><?php echo $movie_data['synopsis']?></span>
+                    <span><?php echo $movie_data['synopsis']?></span><br><br>
+                    <strong>Playing At</strong><br>
+                    <span><?php echo $theatreNamesString ?></span>
                     <div class="book-now-container">
                         <a href="book.php?movie_id=<?php echo $requested_id; ?>" class="book-now-button">Book Now</a>
                     </div>
