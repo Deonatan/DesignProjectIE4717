aa4c89993782dde22e87c43e5c33d3bebb35ce90
hugosebastian <hugosebastian110@gmail.com>
2023-10-31 20:07:30 +0800

    genre sort

--- index.php
+++ index.php
@@ -18,62 +18,114 @@
     exit;
     }
 
-    #Get Selected Sorting Method
+    #Initialize Filter Value
+    $sort_type = 'default';
+    $requested_cinema = 'default';
+    $requested_genre = 'default';
+    $status = 'default'; 
+    #Get Filter Value if POST
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
         $sort_type = $_POST['sort-select'];
-        $selected_theatre = $_POST['theatre-select'];
-    }else{
-        $sort_type = 'default';
-        $selected_theatre = 'all';
+        $requested_cinema = $_POST['theatre-select'];
+        $requested_genre = $_POST['selected-genres'];
     }
 
-    #Retrieve Showing Movies Based of Selected Theatre
-    $showing_movie_id_query = "SELECT movie_id FROM movie_schedule";
-    if ($selected_theatre != 'all') {
-        // If a specific theater is selected, add a WHERE clause to filter by theatre_id
-        $showing_movie_id_query .= " WHERE theatre_id = " . intval($selected_theatre); // Ensure $selected_theatre is properly sanitized.
-    }
-    $showing_movie_id_result = $db->query($showing_movie_id_query);
-    if ($showing_movie_id_result) {
-        $movie_ids = array(); // Create an array to store the movie IDs
-        while ($row = $showing_movie_id_result->fetch_assoc()) {
-            $movie_ids[] = $row['movie_id'];
+    #Get All Movie Id
+    $all_movie_id_query = "SELECT id FROM movie";
+    $all_movie_id_result = $db->query($all_movie_id_query);
+    $all_movie_id_arr = array();
+    if ($all_movie_id_result) {
+        while ($row = $all_movie_id_result->fetch_assoc()) {
+            $all_movie_id_arr[] = $row['id'];
         }
     }
 
-    // Convert the array of movie IDs to a comma-separated string
-    $movie_id_list = implode(', ', $movie_ids);
+    #Get Showing Movie ID
+    $showing_movie_id_query = "SELECT movie_id FROM movie_schedule";
+    $showing_movie_id_result = $db->query($showing_movie_id_query);
+    $showing_movie_id_arr = array();
+    if ($showing_movie_id_result) {
+        while ($row = $showing_movie_id_result->fetch_assoc()) {
+            $showing_movie_id_arr[] = $row['movie_id'];
+        }
+    }
 
-    #Query Movie based on showing movies 
-    $movie_list_query = "SELECT * FROM movie WHERE id IN ($movie_id_list)";
+    // 1. SHOWING / COMING SOON FILTER
+    if ($status=='default'){
+        $movie_id_str = implode(', ', $showing_movie_id_arr);
+    }else{
+        $movie_id_str = implode(', ', array_diff($all_movie_id_arr, $showing_movie_id_arr));
+    }
+    
+    //2.CINEMA FILTER
+    if ($requested_cinema != 'default') {
+        //GET MOVIE ID FROM REQUESTED CINEMA
+        $movie_id_from_schedule = "SELECT movie_id FROM movie_schedule WHERE theatre_id IN($requested_cinema)";
+        // Execute the SQL query
+        $movie_id_from_schedule_result = $db->query($movie_id_from_schedule);
+        // Initialize an array to store movie_id values
+        $movie_id_from_schedule_arr = array();
+        // Check if the query was successful
+        if ($movie_id_from_schedule_result) {
+            // Fetch and add movie_id values to the array
+            while ($row = $movie_id_from_schedule_result->fetch_assoc()) {
+                $movie_id_from_schedule_arr[] = $row['movie_id'];
+            }
+        }
+        $movie_id_str = implode(', ', $movie_id_from_schedule_arr);
+    }
+
+    //3. GENRE FILTER
+    $requested_genre_condition='default';
+    $requested_genre_arr = array();
+    if ($requested_genre!='default'){
+        $requested_genre_arr = explode(',', $requested_genre); // Split the string into an array
+        // Build the genre conditions
+        $requested_genre_condition = [];
+        foreach ($requested_genre_arr as $genre) {
+            $requested_genre_condition[] = "genre LIKE '%" . trim($genre) . "%'";
+        }
+        // Combine the genre conditions with OR
+        $requested_genre_condition = implode(' OR ', $requested_genre_condition);
+    }
+
+    //BUILD MOVIE QUERY
+    $requested_movie_query = "SELECT * FROM movie WHERE id IN ($movie_id_str)";
+    if ($requested_genre_condition!='default') {
+        $requested_movie_query .= " AND ($requested_genre_condition)";
+    }
+
+    //4. Sort Type
     // Modify the query based on the selected sort type
     switch ($sort_type) {
         case 'alphabetical':
-            $movie_list_query .= " ORDER BY title ASC"; // Sort alphabetically by movie title
+            $requested_movie_query .= " ORDER BY title ASC"; // Sort alphabetically by movie title
             break;
-        case 'rating':
-            $movie_list_query .= " ORDER BY rating DESC"; // Sort by rating in descending order
-            break;
-        // For the 'default' case, you don't need to add an ORDER BY clause
-    }
-
-
-    $movie_list_result = $db->query($movie_list_query);
-    $movie_dict = array();
-    $titles = array();
-    if ($movie_list_result){
-        while ($row = $movie_list_result->fetch_assoc()){
-            $movie_dict[$row['title']] = $row;
-            $titles[] = $row['title'];
+            case 'rating':
+                $requested_movie_query .= " ORDER BY rating DESC"; // Sort by rating in descending order
+                break;
+                // For the 'default' case, you don't need to add an ORDER BY clause
+            }
+            
+    // EXECUTE QUERY AND POPULATE DICT
+    $requested_movie_result = $db->query($requested_movie_query);    
+    $requested_movie_dict = array();
+    $requested_movie_titles = array();
+    if ($requested_movie_result){
+        while ($row = $requested_movie_result->fetch_assoc()){
+            $requested_movie_dict[$row['title']] = $row;
+            $requested_movie_titles[] = $row['title'];
+            
         }
     }
     ?>
     <custom-navbar></custom-navbar>
     <table class='movie-list' border="0">
         <tr>
-            <td  colspan="2" class="filter-row">
+            <td  colspan="3" class="filter-row">
             <form id='sort-form'
-            method='POST' onchange="submitForm()" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+            method='POST' 
+            action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
             <div class='filter-container'>
                 <div class="sort-select">
                 <label for="sort-select">Sort by: </label>
@@ -84,9 +136,9 @@
                 </select>
                 </div>
                 <div class="theatre-select">
-                <label for="theatre-select">Select Theatre: </label>
+                <label for="theatre-select">Select Cinema: </label>
                 <select id="theatre-select" name='theatre-select'>
-                    <option value="all" <?php if ($selected_theatre == 'all') echo 'selected'; ?>>All MovieVerse Cinema</option>
+                    <option value="default" <?php if ($requested_cinema == 'default') echo 'selected'; ?>>All MovieVerse Cinema</option>
                     <?php
                     $theatre_list_query = "SELECT * FROM theatre" ;
                     $theatre_list_result = $db->query($theatre_list_query);
@@ -101,32 +153,63 @@
                     foreach ($theatre_name as $name) {
                         $theatre_id = $theatre_dict[$name]['id']; // Get the theater ID from the $theatre_dict array
                         echo '<option value="' . $theatre_id . '" ';
-                        if ($selected_theatre == $theatre_id) {
+                        if ($requested_cinema == $theatre_id) {
                             echo 'selected';
                         }
                         echo '>' . $name . '</option>';
                     }
                     ?>
                 </select>
-            </div>
+                </div>
+                <?php
+                #Retrieve All Movie Genres to create checkbox
+                $genres_list_query = "SELECT GROUP_CONCAT(DISTINCT genre SEPARATOR ', ') AS genres FROM movie;";
+                $genres_list_result = $db->query($genres_list_query)->fetch_assoc();    
+                $genres_array = explode(', ', $genres_list_result['genres']);
+                $genres_array = array_unique($genres_array);
+                sort($genres_array);
+                // echo implode(', ', $genres_array);
+                ?>
+                    <div class="dropdown">
+                    <button class="dropbtn">Genre</button>
+                        <div class="dropdown-content">
+                        <?php
+                        // if (in_array('Action', $requested_genre_arr)){
+                        //     echo "Found";
+                        //   }
+                        //   else {
+                        //     echo "Not Found";
+                        //   }
+                        foreach ($genres_array as $genre) {
+                            // Output a checkbox for each genre
+                            $checked = in_array($genre, $requested_genre_arr) ? 'checked' : '';
+                            echo '
+                            <input type="checkbox" id="' . $genre . '" name="genres[]" value="' . $genre . '" ' . $checked . '>
+                            <label for="' . $genre . '">' . $genre . '</label><br>';
+                        }
+                        ?>
+                        <input type="hidden" id="selected-genres" name="selected-genres" value="">
+                        </div>
+                    </div>
+                <button type='submit' onclick="submitForm()" >Button</button>
             </div>
             </form>
         </td>
     </tr>
         <?php
         $i = 0;
-        while($i<count($movie_dict)){
+        while($i<count($requested_movie_dict)){
             $j=0;
             echo '<tr>';
-            while($i<count($movie_dict) && $j<3){
+            while($i<count($requested_movie_dict) && $j<3){
                 echo '<td>';
-                echo '<a href="php/movie_desc.php?id=' . $movie_dict[$titles[$i]]['id'] . '">';
+                echo '<a href="php/movie_desc.php?id=' . $requested_movie_dict[$requested_movie_titles[$i]]['id'] . '">';
                 echo '  <div class="movie-container">';
-                echo '    <img class="movie-poster" src="'.$movie_dict[$titles[$i]]['poster_link'].'" alt="oppenheimer-poster" style="width: 95%; height:80%;">';
+                echo '    <img class="movie-poster" src="'.$requested_movie_dict[$requested_movie_titles[$i]]['poster_link'].'" alt="oppenheimer-poster" style="width: 95%; height:80%;">';
                 echo '    <div class="movie-info">';
-                echo '      <span>' . $movie_dict[$titles[$i]]['title'] . '</span><br>';
-                echo '      <span>' . $movie_dict[$titles[$i]]['genre'] . '</span><br>';
-                echo '      <span>' . $movie_dict[$titles[$i]]['rating'] . '</span><br>';
+                echo '      <span>' . $requested_movie_dict[$requested_movie_titles[$i]]['title'] . '</span><br>';
+                echo '      <span>' . $requested_movie_dict[$requested_movie_titles[$i]]['genre'] . '</span><br>';
+                echo '      <span>' . $requested_movie_dict[$requested_movie_titles[$i]]['rating'] . '</span><br>';
                 echo '    </div>';
                 echo '  </div>';
                 echo '</a>';
--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 function submitForm() {
     var selectedValue = document.getElementById('sort-select').value;
-    console.log('Hello')
     var form = document.getElementById('sort-form');
+    // Get all selected checkboxes
+    var checkboxes = document.querySelectorAll('input[name="genres[]"]:checked');
+    var selectedValues = [];
+    // Extract the values of selected checkboxes
+    checkboxes.forEach(function(checkbox) {
+        selectedValues.push(checkbox.value);
+    });
+    // Set the hidden input value
+    document.getElementById('selected-genres').value = selectedValues.join(',');
+    // console.log(document.getElementById('selected-genres').value)
     form.submit();
 }
