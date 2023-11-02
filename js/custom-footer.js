354d0b323cc5f6d4ceec954bb50aaecedc70982d
hugosebastian <hugosebastian110@gmail.com>
2023-11-03 01:01:41 +0800

    change vh, footer icon

--- js/custom-footer.js
+++ js/custom-footer.js
@@ -6,16 +6,25 @@ class Footer extends HTMLElement {
     connectedCallback() {
       const type = this.getAttribute("type");
       let content = `
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
       <style>
       .footer-container{
         // margin-top:30px;
         background-color: #060E2A;
         text-align:center;
         color:white;
-        height:50px;
+        height:80px;
         display: flex;
         justify-content: center; /* Center horizontally */
         align-items: center; /* Center vertically */
+      }
+
+      .icon i {
+        color: white;
+      }
+
+      a{
+        margin-top:5px;
       }
 
       #mail-href{
@@ -28,8 +37,13 @@ class Footer extends HTMLElement {
         <div class='footer-container'>
         <small><i>
             Copyright &copy; 2023 Movie Verse<br>
-            <a id='mail-href' href="mailto:movieverse@email.com">movieverse@email.com</a>
-        </i></small>
+            <a id='mail-href' href="mailto:movieverse@email.com">movieverse@email.com</a><br>
+            <div style='margin-top:5px;'>
+            <a href="#" class="icon"><i class="fab fa-facebook"></i></a>
+            <a href="#" class="icon"><i class="fab fa-twitter"></i></a>
+            <a href="#" class="icon"><i class="fab fa-instagram"></i></a>
+            </div>
+        </i></small><br>
         </div>
         </footer>
   </body>
