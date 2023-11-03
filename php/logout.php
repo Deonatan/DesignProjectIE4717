afe0c019f50d7e5bff35499b4ba3358b08578a1a
Deonatan <deonatan27@gmail.com>
2023-11-03 11:42:05 +0800

    Redirect logout

--- php/logout.php
+++ php/logout.php
@@ -1,4 +1,15 @@
 <?php 
 session_start();
 session_destroy(); 
+echo '<script>
+if (typeof(Storage) !== "undefined") {
+    localStorage.clear();
+    console.log("localStorage cleared");
+} else {
+    console.log("localStorage is not supported.");
+}
+</script>';
+$redirect_url = isset($_SESSION["redirect_url"]) ? $_SESSION["redirect_url"] : "../index.php";
+header("Location: " . $redirect_url);
+exit;
 ?>
